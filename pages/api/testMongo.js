ded67fffbd94b3b420f0252638d1f5bae5fec4c1
Mala Patke <ashahid@ucsb.edu>
2024-01-13 16:47:47 -0800

    Formatting

--- pages/api/testMongo.js
+++ pages/api/testMongo.js
@@ -1,13 +1,12 @@
 import mongoclient from '../../lib/mongodb';
 
 /**
- * @param req {import('next').NextApiRequest}
- * @param res {import('next'.NextApiResponse)}s
+ * @param {import('next').NextApiRequest} req 
+ * @param {import('next').NextApiResponse} res 
  */
 export default async (req, res) => {
     let client = await mongoclient.connect();
     let db = client.db('SBHacks2024');
-    await db.createCollection("MyReallyCoolCollection");
 
     res.send("if you're seeing this its working yipee")
 }
